cb45b10c9c23181def2b14b514c3d9b17f20cf21
aybhghggf <ayoubehamadi5@gmail.com>
2025-07-27 15:52:39 +0100

    form exercice

--- react-tailwind-app/src/App.js
+++ react-tailwind-app/src/App.js
@@ -1,23 +1,34 @@
-import { click } from "@testing-library/user-event/dist/click";
-import { useState } from "react";
-
-export default function App() {
-  const [click, setClick] = useState(0);
-  function handleClick() {
-    setClick(click + 1);
-  }
-  function resetClick() {
-    setClick(0);
-  }
-  return (
-    <>
-      <button onClick={handleClick}>Click me</button>
+import React, { useState } from 'react';
 
 
-      <button onClick={resetClick}>Reset </button>
 
-      <p>Vous avez cliker:{click} </p>
+export default function MyInput() {
+    const [inputValue, setInputValue] = useState('');
 
-    </>
-  )
+    function handleChange(event) {
+        setInputValue(event.target.value);
+        console.log("Input value changed:", event.target.value);
+    }
+    function submitForm(){
+        if(inputValue.length > 0){
+            console.log("Form submitted with value:", inputValue);
+            setInputValue(''); 
+        }else{
+            console.log("Input is empty, please enter a value.");
+        }
+    }
+
+    return (
+      <div >
+             <input
+            type="text"
+            value={inputValue}
+            onChange={handleChange}
+            className="border border-gray-300 rounded py-2 px-4"
+            placeholder="Type something..."
+        />
+        <button onClick={submitForm}>Submit</button>
+      </div>
+
+    );
 }
