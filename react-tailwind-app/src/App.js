df7fe50af016126fa43c17373b831a43750019ac
aybhghggf <ayoubehamadi5@gmail.com>
2025-07-29 19:23:09 +0100

    array exercie

--- react-tailwind-app/src/App.js
+++ react-tailwind-app/src/App.js
@@ -1,49 +1,41 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+
 
 export default function App() {
-  const [produit, setProduit] = useState([]);
-  const [loading, setLoading] = useState(true);
+  const array = [
+    "Ayoube", "Zakaria", "Alaoui", "Amine", "Ibrahim", "Ahmed"
+  ];
 
-  useEffect(() => {
-    fetch("https://fake-store-api.mock.beeceptor.com/api/products")
-      .then((response) => response.json())
-      .then((data) => {
-        setProduit(data);
-        setLoading(false);
-      })
-      .catch((error) => {
-        alert("Erreur");
-        console.error(error);
-        setLoading(false);
-      });
-  }, []);
+  const [Value,setValue] = useState();
+  const [students,setStudents]= useState([
+      ...array
+  ])
 
-  function LoadItems() {
-    return produit.map((prod) => (
-      <div className="row" key={prod.id}>
-        <div className="card">
-          <div className="card-header">
-            <img src={prod.image} alt={prod.title} />
-          </div>
-          <div className="card-body">
-            <h1>{prod.title}</h1>
-            <p>{prod.description}</p>
-          </div>
-          <div className="card-footer">
-            <button className="btn btn-primary">Voir</button>
-          </div>
-        </div>
-      </div>
-    ));
+
+
+  function displayStudents() {
+    return (
+      <ul>
+        {students.map((student, index) => (
+          <li key={index}>{student}</li>
+        ))}
+      </ul>
+    );
+  }
+  function submtiForm(e){
+    e.preventDefault();
+    setStudents([...students,Value]);
+    setValue("");
   }
 
   return (
     <>
-      {loading ? (
-        <p className="text-center h-14 w-14">Chargement...</p>
-      ) : (
-        <LoadItems />
-      )}
+          {displayStudents()}
+      <form onSubmit={submtiForm}>
+        <input type="text" placeholder="Ajoutez un nom"  value={Value} onChange={(e)=>{setValue(e.target.value)}}/>
+        <button className="btn btn-primary" >Ajouter</button>
+      </form>
+
     </>
   );
 }
