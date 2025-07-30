0ea517e39007310ef4ff2dcfc2a309014429299f
aybhghggf <ayoubehamadi5@gmail.com>
2025-07-30 16:03:12 +0100

    add+ delete on array

--- react-tailwind-app/src/App.js
+++ react-tailwind-app/src/App.js
@@ -1,41 +1,49 @@
 import { useState } from "react";
 
-
 export default function App() {
-  const array = [
-    "Ayoube", "Zakaria", "Alaoui", "Amine", "Ibrahim", "Ahmed"
-  ];
+  const [valueofInput, setvalueofInput] = useState("");
+  const [students, setStudents] = useState(["ayoube"]);
 
-  const [Value,setValue] = useState();
-  const [students,setStudents]= useState([
-      ...array
-  ])
-
-
-
-  function displayStudents() {
-    return (
-      <ul>
-        {students.map((student, index) => (
-          <li key={index}>{student}</li>
-        ))}
-      </ul>
-    );
+  function ochnageevent(e) {
+    setvalueofInput(e.target.value);
   }
-  function submtiForm(e){
+
+  function onsubmitform(e) {
     e.preventDefault();
-    setStudents([...students,Value]);
-    setValue("");
+    if (valueofInput.trim() === "") return;
+    setStudents([...students, valueofInput]);
+    setvalueofInput(""); 
+  }
+
+  function deleteOnArray(IndexToDelete) {
+    const nouveauTable = students.filter((_, index) => index !== IndexToDelete);
+    setStudents(nouveauTable);
   }
 
   return (
     <>
-          {displayStudents()}
-      <form onSubmit={submtiForm}>
-        <input type="text" placeholder="Ajoutez un nom"  value={Value} onChange={(e)=>{setValue(e.target.value)}}/>
-        <button className="btn btn-primary" >Ajouter</button>
-      </form>
+      <ul>
+        {students.map((student, index) => (
+          <li key={index}>
+            {student}{" "}
+            <button onClick={() => deleteOnArray(index)}>Supprimer</button>
+          </li>
+        ))}
+      </ul>
 
+      <div className="Input">
+        <form onSubmit={onsubmitform}>
+          <input
+            type="text"
+            placeholder="Ajouter un étudiant"
+            value={valueofInput}
+            onChange={ochnageevent}
+          />
+          <button type="submit" className="btn btn-primary m-2">
+            Ajoutez
+          </button>
+        </form>
+      </div>
     </>
   );
 }
